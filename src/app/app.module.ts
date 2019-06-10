8b26209fd849740070c24c764b29905633482d56
Slavik Teleshenko <viachik.slav@gmail.com>
2019-06-10 12:13:39 +0300

    clarity deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ClarityModule} from "@clr/angular";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    ClarityModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
